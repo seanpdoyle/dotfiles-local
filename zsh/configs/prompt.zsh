8c47dfc7eeb5ca011876d107f134d7480af3616f
Sean Doyle <sean.p.doyle24@gmail.com>
2015-12-19 18:39:47 -0500

    Fix prompt to work with new dotfiles
    [This commit to thoughtbot/dotfiles][commit] overrode/broke my ZSH prompt.
    
    This commit corrects my ZSH configuration.
    
    [commit]: https://github.com/thoughtbot/dotfiles/commit/31b6ad4f3fd70732ae16b7093b4e4b8705b4a608

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -8,12 +8,10 @@ git_branch() {
 
 git_dirty() {
   st=$(git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
+  if [[ $st == "" ]]; then
     echo ""
   else
-    if [[ $st == "nothing to commit, working directory clean" ]]
-    then
+    if [[ $st == "nothing to commit, working directory clean" ]]; then
       echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     else
       echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
@@ -23,7 +21,7 @@ git_dirty() {
 
 git_prompt_info () {
  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+
  echo "${ref#refs/heads/}"
 }
 
@@ -32,8 +30,7 @@ unpushed () {
 }
 
 need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
+  if [[ $(unpushed) == "" ]]; then
     echo " "
   else
     echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
@@ -41,28 +38,8 @@ need_push () {
 }
 
 rb_prompt(){
-  if $(which rbenv &> /dev/null)
-  then
+  if $(which rbenv &> /dev/null); then
     echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-  else
-    echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
   else
     echo ""
   fi
@@ -72,12 +49,5 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+NEWLINE=$'\n'
+PS1="${NEWLINE}$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)${NEWLINE}› "
